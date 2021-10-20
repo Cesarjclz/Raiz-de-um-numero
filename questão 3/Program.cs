b40bc71ad85dd93e6d10e5981ee6c95319f3f543
César Aguiar <Cesarphz21@gmail.com>
2021-10-20 12:16:44 -0300

    Adicionar arquivos de projeto.

---
+++ questão 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace questão_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+            
+            double nu;
+            
+            Console.WriteLine("Digite um numero: ");
+            nu = double.Parse(Console.ReadLine());
+            if (nu >= 0)
+            {
+                nu = Convert.ToSingle(Math.Sqrt(nu));
+                Console.WriteLine("A raiz do numero é: {0}", nu);
+            }
+            else if (nu < 0)
+            {
+                nu = Math.Pow(nu, 2);
+                Console.WriteLine("O quadrado do numero é: " + nu);
+
+            }
+        }
+    }
+}
